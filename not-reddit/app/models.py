019f053dd25e39d8013607d1e02c4cbe0808868c
Adam Santaniello <adam.santaniello@icloud.com>
2024-03-11 16:48:46 -0400

    Create Post model

--- not-reddit/app/models.py
+++ not-reddit/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django_softdelete.models import SoftDeleteModel
 
 # Create your models here.
+
+class Post(SoftDeleteModel):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
